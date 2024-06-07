411d7b9888e18bc5dc4d3fb318cb986ca4cdacbf
Manthan Gaikwad <87421766+ManthanG369@users.noreply.github.com>
2024-06-07 15:37:39 +0530

    data types

---
+++ 01_Basics/02_datatype.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3); // code readability should be high
+
+console.log("manthan");
+
+let name = "Manthan";
+let age = 25;
+let isLoggedin = true;
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+console.log(typeof name); // string;
+console.log(typeof age); // number;
+console.log(typeof isLoggedin); // boolean
+
+// object
+
+console.log(typeof state); // undefined
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object

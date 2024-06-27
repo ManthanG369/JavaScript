dab92212726ff87d98976f540e16e34185c29d45
Manthan Gaikwad <87421766+ManthanG369@users.noreply.github.com>
2024-06-27 11:52:45 +0530

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,48 @@
+let score = NaN;
+
+// console.log("score:", score);
+// console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log("valueInNumber:", valueInNumber);
+
+// console.log("valueInNumber:type", typeof valueInNumber);
+
+// * * "33" => 33
+// * "33abc" => NaN
+// * true => 1; false => 0
+// * undefined =>  NaN
+// * null // => 0
+// * "hello" // => NaN
+// * ``  // => 0
+// *  Nan // => NaN
+
+let isLoggedin = "";
+// console.log("isLoggedin:", isLoggedin);
+// console.log("isLoggedin:type", isLoggedin);
+
+let booleanIsLoggedin = Boolean(isLoggedin);
+// console.log("booleanIsLoggedin:", booleanIsLoggedin);
+// console.log("booleanIsLoggedin:type", booleanIsLoggedin);
+
+// * 1 => true; 0 => false
+// * "" => false
+// * "hitesh" => true
+// * undefined => false
+// * NaN => false
+// * null => false
+
+let someNumber = 23;
+console.log("someNumber:", someNumber);
+console.log("someNumber:type ", typeof someNumber);
+
+let stringNumber = String(someNumber);
+console.log("stringNumber:", stringNumber);
+console.log("stringNumber:type", typeof stringNumber);
+
+// * 23 => string
+// *
+// *
+// *
+// *

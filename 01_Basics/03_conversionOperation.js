22d5fb6fbe490a579289125a59f2a22a5f5f4749
Manthan Gaikwad <87421766+ManthanG369@users.noreply.github.com>
2024-07-01 10:51:26 +0530

    conversion of datatypes with increment prefix and postfix

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -33,16 +33,82 @@ let booleanIsLoggedin = Boolean(isLoggedin);
 // * NaN => false
 // * null => false
 
-let someNumber = 23;
-console.log("someNumber:", someNumber);
-console.log("someNumber:type ", typeof someNumber);
+let someNumber = ``;
+// console.log("someNumber:", someNumber);
+// console.log("someNumber:type ", typeof someNumber);
 
 let stringNumber = String(someNumber);
-console.log("stringNumber:", stringNumber);
-console.log("stringNumber:type", typeof stringNumber);
+// console.log("stringNumber:", stringNumber);
+// console.log("stringNumber:type", typeof stringNumber);
 
 // * 23 => string
-// *
-// *
-// *
-// *
+// * true/false => string
+// * undefined => string
+// * null=>  string
+// * NaN => string
+// * `` => string
+
+// ! *********************************** Operations ***********************************
+
+let value = ``;
+let negValue = -value;
+// console.log("negValue:", negValue);
+// console.log("negValue: type", typeof negValue);
+// * value     === optput    type
+
+// * 2         ==> -2       number
+// * "manthan" ==> NaN      number
+// * true      ==> -1       number
+// * false     ==> -0       number
+// * undefined ==> NaN      number
+// * null      ==> -0       number
+// * ``        ==> -0       number
+
+// * console.log(2+2)   // 2
+// * console.log(2+2); ==> 4
+// * console.log(2-2);  ==>0
+// * console.log(2*2);  ==> 4
+// * console.log(2**3); ==6
+// * console.log(2 / 3);    ==>0.6
+// * console.log("2 % 3", 2 % 3);  ==> 2 is remender
+
+let str1 = "hello";
+let str2 = " hitesh";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// * console.log("1" + 2);            ==> 12
+// * console.log(1 + "2");            ==> 12
+// * console.log("1" + 2 + 2);        ==> 122
+// * console.log(1 + 2 + "2");        ==> 32
+
+// * console.log(((3 + 4) * 5) % 3);  ==> 2
+
+// * console.log(+true);              ==> 1
+// * console.log(+"");                ==> 0
+
+// * console.log(+true);                   ==> 1
+// * console.log(+" ");                    ==> 0
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+// console.log("gameCounter:", gameCounter); // * 101
+
+let x = 3;
+const y = x++;
+// ! expected is x = 3 and y = 4
+console.log(`x: ${x} y :${y}`); // * x:4 y:3
+
+// ? If used postfix, with operator after operand (for example, x++),
+// ?  the increment operator increments and returns the value before incrementing.
+0;
+// ? If used prefix, with operator before operand (for example, ++x),
+// ? the increment operator increments and returns the value after incrementing.
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion

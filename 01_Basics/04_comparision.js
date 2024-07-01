af325f53d59c6f418e1768722aa9dcc1424e3c06
Manthan Gaikwad <87421766+ManthanG369@users.noreply.github.com>
2024-07-01 11:03:09 +0530

    comparision

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0); // * false
+console.log(null == 0); // * false
+console.log(null >= 0); // * true   null is converted in 0
+
+console.log(undefined == 0); //* false
+console.log(undefined > 0); //*  false
+console.log(undefined < 0); //*  false
+
+// ===
+
+console.log("2" === 2); // *  false  === check both side datatype and value
